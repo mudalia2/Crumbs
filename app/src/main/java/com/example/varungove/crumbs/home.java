be2817850686fa0c66104e7c357640eacb67f55d
varungove <varun.goverdhan@gmail.com>
2015-12-06 12:51:32 -0600

    Settings Page

--- app/src/main/java/com/example/varungove/crumbs/home.java
+++ app/src/main/java/com/example/varungove/crumbs/home.java
@@ -38,4 +38,10 @@ public class home extends AppCompatActivity{
         Intent intent = new Intent(home.this, mail.class);
         startActivity(intent);
     }
+
+    public void settings(View view)
+    {
+        Intent intent = new Intent(home.this, settings.class);
+        startActivity(intent);
+    }
 }

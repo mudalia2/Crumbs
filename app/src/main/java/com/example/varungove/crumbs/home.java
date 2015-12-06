9b5222a4b2d317e27f66b7de1f83bc2f96de09ff
varungove <varun.goverdhan@gmail.com>
2015-12-06 01:39:57 -0600

    Home Screen Basic Layout

---
+++ app/src/main/java/com/example/varungove/crumbs/home.java
@@ -0,0 +1,41 @@
+package com.example.varungove.crumbs;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class home extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        MyPagerAdapter adapter = new MyPagerAdapter();
+        ViewPager myPager = (ViewPager) findViewById(R.id.viewpager_layout);
+        myPager.setAdapter(adapter);
+        myPager.setCurrentItem(1);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void mail(View view)
+    {
+        Intent intent = new Intent(home.this, mail.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/varungove/crumbs/mail.java
+++ app/src/main/java/com/example/varungove/crumbs/mail.java
@@ -1,36 +1,27 @@
 package com.example.varungove.crumbs;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class mail extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mail);
+
 
     }
-
-    public void signup(View view)
-    {
-        Intent intent = new Intent(MainActivity.this, signup.class);
-        startActivity(intent);
-    }
-
-    public void home(View view)
-    {
-        Intent intent = new Intent(MainActivity.this, home.class);
-        startActivity(intent);
-    }
-
 
 
 }

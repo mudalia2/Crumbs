b570d3be4036d8218cae58b46440d63e7f2094e1
varungove <varun.goverdhan@gmail.com>
2015-12-07 00:57:36 -0600

    Basic Layout of All screens

--- app/src/main/java/com/example/varungove/crumbs/home.java
+++ app/src/main/java/com/example/varungove/crumbs/home.java
@@ -12,6 +12,10 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class home extends AppCompatActivity{
 
@@ -19,10 +23,26 @@ public class home extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        //Image Swiping
         MyPagerAdapter adapter = new MyPagerAdapter();
         ViewPager myPager = (ViewPager) findViewById(R.id.viewpager_layout);
         myPager.setAdapter(adapter);
         myPager.setCurrentItem(1);
+
+        //Trying to open resinfo through longpress on images
+//        LinearLayout txtView = (LinearLayout) findViewById(R.id.pics);
+//        txtView.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//
+//                Intent intent = new Intent(home.this, resinfo.class);
+//                startActivity(intent);
+//                return true;
+//
+//            }
+//
+//        });
 
     }
 
@@ -48,6 +68,12 @@ public class home extends AppCompatActivity{
     public void invite(View view)
     {
         Intent intent = new Intent(home.this, invite.class);
+        startActivity(intent);
+    }
+
+    public void preferences(View view)
+    {
+        Intent intent = new Intent(home.this, preferences.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/varungove/crumbs/preferences.java
+++ app/src/main/java/com/example/varungove/crumbs/preferences.java
@@ -5,24 +5,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class signup extends AppCompatActivity{
+public class preferences extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup);
+        setContentView(R.layout.preferences);
+
 
     }
 
-    public void home(View view)
-    {
-        Intent intent = new Intent(signup.this, home.class);
-        startActivity(intent);
-    }
+
 }

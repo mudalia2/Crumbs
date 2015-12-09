844ec8fb4061b23817ab481ce3c7c142e55290f1
varungove <varun.goverdhan@gmail.com>
2015-12-09 14:44:59 -0600

    Addeded functionality to Mail screen (Accept/Decline invitations)

--- app/src/main/java/com/example/varungove/crumbs/mail.java
+++ app/src/main/java/com/example/varungove/crumbs/mail.java
@@ -8,10 +8,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class mail extends AppCompatActivity{
 
@@ -29,6 +37,64 @@ public class mail extends AppCompatActivity{
         Intent intent = new Intent(mail.this, settings.class);
         startActivity(intent);
     }
+
+    public void accept(View view)
+    {
+        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+        dlgAlert.setMessage("Invitation Accepted");
+        dlgAlert.setPositiveButton("OK", null);
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
+
+        dlgAlert.setPositiveButton("Ok",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //dismiss the dialog
+                    }
+                });
+
+        switch (view.getId())
+        {
+            case(R.id.yes1):
+                RelativeLayout x = (RelativeLayout) findViewById(R.id.invite);
+                x.setVisibility(View.INVISIBLE);
+                break;
+            case(R.id.yes):
+                RelativeLayout y = (RelativeLayout) findViewById(R.id.invite1);
+                y.setVisibility(View.INVISIBLE);
+                break;
+        }//end of switch
+    }//end of accept dialog
+
+    public void decline(View view)
+    {
+        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+        dlgAlert.setMessage("Invitation Declined");
+        dlgAlert.setPositiveButton("OK", null);
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
+
+        dlgAlert.setPositiveButton("Ok",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //dismiss the dialog
+                    }
+                });
+
+            switch (view.getId())
+            {
+                case(R.id.no1):
+                    RelativeLayout x = (RelativeLayout) findViewById(R.id.invite);
+                    x.setVisibility(View.INVISIBLE);
+                        break;
+                case(R.id.no):
+                    RelativeLayout y = (RelativeLayout) findViewById(R.id.invite1);
+                    y.setVisibility(View.INVISIBLE);
+                    break;
+            }//end of switch
+
+    }//end of accept dialog
+
 
 
 }

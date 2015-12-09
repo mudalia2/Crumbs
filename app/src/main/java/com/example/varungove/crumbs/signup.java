702402a77b3be30a47f10b74e86a97abba6d7649
varungove <varun.goverdhan@gmail.com>
2015-12-09 15:24:04 -0600

    formatted settings page

--- app/src/main/java/com/example/varungove/crumbs/signup.java
+++ app/src/main/java/com/example/varungove/crumbs/signup.java
@@ -1,6 +1,8 @@
 package com.example.varungove.crumbs;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -22,6 +24,20 @@ public class signup extends AppCompatActivity{
 
     public void home(View view)
     {
+
+        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+        dlgAlert.setMessage("Account Created");
+        dlgAlert.setPositiveButton("OK", null);
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
+
+        dlgAlert.setPositiveButton("Ok",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //dismiss the dialog
+                    }
+                });
+
         Intent intent = new Intent(signup.this, home.class);
         startActivity(intent);
     }

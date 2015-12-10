26eaccb861ca5a71089a4acd166c1f32ff01038c
Ankush Aggarwal <ankush.aggarwl@gmail.com>
2015-12-10 13:03:49 -0600

    New pages etc

--- app/src/main/java/com/example/varungove/crumbs/home.java
+++ app/src/main/java/com/example/varungove/crumbs/home.java
@@ -1,20 +1,27 @@
 package com.example.varungove.crumbs;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class home extends AppCompatActivity{
@@ -31,18 +38,6 @@ public class home extends AppCompatActivity{
         myPager.setCurrentItem(1);
 
         //Trying to open resinfo through longpress on images
-//        LinearLayout txtView = (LinearLayout) findViewById(R.id.pics);
-//        txtView.setOnLongClickListener(new View.OnLongClickListener() {
-//            @Override
-//            public boolean onLongClick(View v) {
-//
-//                Intent intent = new Intent(home.this, resinfo.class);
-//                startActivity(intent);
-//                return true;
-//
-//            }
-//
-//        });
 
     }
 
@@ -75,5 +70,60 @@ public class home extends AppCompatActivity{
     {
         Intent intent = new Intent(home.this, preferences.class);
         startActivity(intent);
+    }
+
+    public class MyPagerAdapter extends PagerAdapter {
+        public int getCount() {
+            return 3;
+        }
+        public Object instantiateItem(ViewGroup collection, int position) {
+            LayoutInflater inflater = (LayoutInflater) collection.getContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            int resId = 0;
+            switch (position) {
+                case 0:
+                    resId = R.layout.swipe1;
+                    break;
+                case 1:
+                    resId = R.layout.swipe2;
+                    break;
+                case 2:
+                    resId = R.layout.swipe3;
+                    break;
+
+
+
+            }
+            View view = inflater.inflate(resId, null);
+
+            view.setOnLongClickListener(new View.OnLongClickListener() {
+                public boolean onLongClick(View v) {
+                    //this will log the page number that was click
+                    Intent intent = new Intent(home.this, resinfo.class);
+                    startActivity(intent);
+                    return true;
+                }
+            });
+            ((ViewPager) collection).addView(view, 0);
+
+            return view;
+        }
+        @Override
+        public void destroyItem(ViewGroup arg0, int arg1, Object arg2) {
+            ((ViewPager) arg0).removeView((View) arg2);
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == ((View) arg1);
+        }
+
+        //public boolean isViewFromObject(ViewGroup arg0, Object arg1) {
+        // return arg0 == ((View) arg1);
+        //}
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/varungove/crumbs/invite.java
+++ app/src/main/java/com/example/varungove/crumbs/invite.java
@@ -1,6 +1,11 @@
 package com.example.varungove.crumbs;
 
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,16 +17,125 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class invite extends AppCompatActivity{
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView, timeView;
+    private int year, month, day, hour, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.invite);
 
+        dateView = (TextView) findViewById(R.id.dateView);
+        timeView = (TextView) findViewById(R.id.timeView);
+        calendar = Calendar.getInstance();
 
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+        minute = calendar.get(Calendar.MINUTE);
+
+        showTime(hour, minute + 30);
+        showDate(year, month + 1, day);
     }
 
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+    }
 
+    private void showTime(int hour, int minute) {
+        if (minute < 30)
+            timeView.setText(new StringBuilder().append(pad(hour)).append(":")
+                .append(pad(minute)));
+        else if (hour < 23)
+            timeView.setText(new StringBuilder().append(pad(hour + 1)).append(":")
+                    .append(pad(minute - 60)));
+        else
+            timeView.setText(new StringBuilder().append(pad(hour - 23)).append(":")
+                    .append(pad(minute - 60)));
+    }
+
+    public void setDate(View view) {
+        showDialog(999);
+    }
+
+    public void setTime(View view) {
+        showDialog(1000);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case 999:
+                return new DatePickerDialog(this, pickerListener1, year, month,day);
+            case 1000:
+                return new TimePickerDialog(this, pickerListener2, hour, minute, false);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener pickerListener1 = new DatePickerDialog.OnDateSetListener() {
+
+        // when dialog box is closed, below method will be called.
+        @Override
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+
+            year  = selectedYear;
+            month = selectedMonth;
+            day   = selectedDay;
+
+            // Show selected date
+            dateView.setText(new StringBuilder().append(month + 1)
+                    .append("/").append(day).append("/").append(year));
+
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener pickerListener2 =
+            new TimePickerDialog.OnTimeSetListener() {
+                public void onTimeSet(TimePicker view, int selectedHour,
+                                      int selectedMinute) {
+                    hour = selectedHour;
+                    minute = selectedMinute;
+
+                    // set current time into textview
+                    timeView.setText(new StringBuilder().append(pad(hour))
+                            .append(":").append(pad(minute)));
+
+                }
+            };
+
+    private static String pad(int c) {
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
+    }
+
+    public void sendInvites(View view)
+    {
+        new AlertDialog.Builder(this)
+                .setTitle("Invite")
+                .setMessage("Invites Sent Successfully")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(invite.this, home.class);
+                        startActivity(intent);                    }
+                })
+                .show();
+
+    }
 }
+

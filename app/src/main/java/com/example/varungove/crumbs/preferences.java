1e05c98d4ab0ccfa4e127bc73e989f1a8777b0b4
varungove <varun.goverdhan@gmail.com>
2015-12-10 14:39:15 -0600

    preferneces done

--- app/src/main/java/com/example/varungove/crumbs/preferences.java
+++ app/src/main/java/com/example/varungove/crumbs/preferences.java
@@ -2,6 +2,7 @@ package com.example.varungove.crumbs;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,6 +18,7 @@ import android.widget.TextView;
 public class preferences extends AppCompatActivity{
 
     TextView p1, p2, p3;
+    TextView d1, d2, d3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -26,24 +28,50 @@ public class preferences extends AppCompatActivity{
         p2 = (TextView) findViewById(R.id.dollar2);
         p3 = (TextView) findViewById(R.id.dollar3);
 
+        d1 = (TextView) findViewById(R.id.distance1);
+        d2 = (TextView) findViewById(R.id.distance2);
+        d3 = (TextView) findViewById(R.id.distance3);
+
     }
 
     public void price1(View view) {
-        p1.setBackgroundColor(0xf6f1eb00);
-        p2.setBackgroundColor(0xdfcdb600);
-        p3.setBackgroundColor(0xdfcdb600);
+        p1.setBackgroundColor(Color.parseColor("#e55050"));
+        p2.setBackgroundColor(Color.parseColor("#cfb593"));
+        p3.setBackgroundColor(Color.parseColor("#cfb593"));
+
     }
 
     public void price2(View view) {
-        p1.setBackgroundColor(0xdfcdb600);
-        p2.setBackgroundColor(0xf6f1eb00);
-        p3.setBackgroundColor(0xdfcdb600);
+        p2.setBackgroundColor(Color.parseColor("#e55050"));
+        p1.setBackgroundColor(Color.parseColor("#cfb593"));
+        p3.setBackgroundColor(Color.parseColor("#cfb593"));
+
     }
 
     public void price3(View view) {
-        p1.setBackgroundColor(0xdfcdb600);
-        p2.setBackgroundColor(0xdfcdb600);
-        p3.setBackgroundColor(0xf6f1eb00);
+        p3.setBackgroundColor(Color.parseColor("#e55050"));
+        p2.setBackgroundColor(Color.parseColor("#cfb593"));
+        p1.setBackgroundColor(Color.parseColor("#cfb593"));
+    }
+
+    public void dis1(View view) {
+        d1.setBackgroundColor(Color.parseColor("#e55050"));
+        d2.setBackgroundColor(Color.parseColor("#cfb593"));
+        d3.setBackgroundColor(Color.parseColor("#cfb593"));
+
+    }
+
+    public void dis2(View view) {
+        d2.setBackgroundColor(Color.parseColor("#e55050"));
+        d1.setBackgroundColor(Color.parseColor("#cfb593"));
+        d3.setBackgroundColor(Color.parseColor("#cfb593"));
+
+    }
+
+    public void dis3(View view) {
+        d3.setBackgroundColor(Color.parseColor("#e55050"));
+        d2.setBackgroundColor(Color.parseColor("#cfb593"));
+        d1.setBackgroundColor(Color.parseColor("#cfb593"));
     }
 
 }
